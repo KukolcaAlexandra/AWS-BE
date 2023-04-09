b9401e67bc80008ccad3050853ff69c6ecec0eef
Alexandra Kukolca <alexandra_kukolca1@epam.com>
2023-04-09 23:06:59 +0300

    [task-4]: add script to fill db tables

--- product-service/service/mock-data.js
+++ product-service/service/mock-data.js
@@ -1,14 +1,32 @@
 export const mockProductsData = [
   {
-    id: 1,
     title: 'Tools',
+    description: 'The best tool',
     price: 114,
-    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elfadistrelec.no%2Fen%2Ftoolset-110-part-110-pieces-crescent-ctk110eu%2Fp%2F18041036&psig=AOvVaw03vgnu5vD2wqeNsAEvUvwf&ust=1680817219246000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLDMpZzak_4CFQAAAAAdAAAAABAJ',
   },
   {
-    id: 2,
     title: 'Drill',
+    description: 'The best Drill',
     price: 53,
-    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FGardenJoy-Cordless-Speed%25EF%25BC%25880-1500-Accessories-Improvement%2Fdp%2FB08QCG6NKR&psig=AOvVaw0uSsoBp5nv2hVh1EG-ZeAV&ust=1680817266470000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDGzbLak_4CFQAAAAAdAAAAABAE',
   },
+  {
+    title: "Screwdriver",
+    description: 'The best Screwdriver',
+    price: 55,
+  }
+];
+
+export const mockStocksData = [
+  {
+    product_id: 'c4266d00-d70e-11ed-a48d-bb3cffe6f8db',
+    count: 3,
+  },
+  {
+    product_id: 'c426e230-d70e-11ed-a48d-bb3cffe6f8db',
+    count: 5,
+  },
+  {
+    product_id: 'c426e231-d70e-11ed-a48d-bb3cffe6f8db',
+    count: 15,
+  }
 ];
